55c0d9252832e47654d3b536c3f20d9176590341
Gabriel Araujo <gabrielsant95@gmail.com>
2024-03-09 00:46:39 -0300

    New change on basic variables

--- python_crash_course_exercises/exercise3.py
+++ python_crash_course_exercises/exercise3.py
@@ -2,3 +2,5 @@ famous_person = "Jordan peterson"
 quote = "Don’t be a slave to stupid rules."
 
 print(f'{famous_person.title()} once said, "{quote}"')
+stripped_name = famous_person.strip("J")
+print(stripped_name)

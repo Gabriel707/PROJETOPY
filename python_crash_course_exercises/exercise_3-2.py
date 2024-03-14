0135ebaa96de6658a22051ab093fdc9426b65427
Gabriel Araujo <gabrielsant95@gmail.com>
2024-03-13 23:28:02 -0300

    Working with lists

---
+++ python_crash_course_exercises/exercise_3-2.py
@@ -0,0 +1,7 @@
+list_of_users = ["sysadmin", "serviceadmin",
+                 "dbalevel1", "dbalevel2", "dbalevel3"]
+
+greetings = "Hello"
+
+for user in list_of_users:
+    print(f'{greetings}, {user}')

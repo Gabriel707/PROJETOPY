b0b002b32392410a0999afd6d9b1ab659ce0d3e5
Gabriel Araujo <gabrielsant95@gmail.com>
2024-03-13 23:35:35 -0300

    implementing...

--- python_crash_course_exercises/exercise_3-2.py
+++ python_crash_course_exercises/exercise_3-2.py
@@ -4,4 +4,23 @@ list_of_users = ["sysadmin", "serviceadmin",
 greetings = "Hello"
 
 for user in list_of_users:
-    print(f'{greetings}, {user}')
+    print(f'{greetings}, {user}\n')
+
+print(list_of_users)
+
+list_of_users[0] = 'sysadmin_np'
+print(list_of_users)
+
+list_of_users.append('cbs_non_prod')
+print(list_of_users)
+
+list_of_users.insert(0, 'master_admin')
+print(list_of_users)
+
+del list_of_users[0]
+print(list_of_users)
+
+# Need to find a way to indentify what's element position to be popped - to be implemented
+print(list_of_users)
+removed_user = list_of_users.pop(-1)
+print(removed_user)

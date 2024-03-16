1055862602634ebc6df84c725e119fbe05d4b056
Gabriel Araujo <gabrielsant95@gmail.com>
2024-03-16 00:55:34 -0300

    starting testing with pytest. TBC

---
+++ tests/test_list_processing.py
@@ -0,0 +1,11 @@
+import importlib
+from python_crash_course_exercises import exercise_4
+
+
+def test_if_testedvalue_is_part_of_lista_partI():
+    entrada_num_a_ser_testado = int(input('N° a ser testado: '))
+    
+    if entrada_num_a_ser_testado in:
+
+
+    assert resultado == esperado

c14acf8ccdd68a6959b865a2d0725572f02d3a97
Gabriel Araujo <gabrielsant95@gmail.com>
2024-03-21 21:19:29 -0300

    Playing around with classes.

---
+++ python_crash_course_exercises/musica.py
@@ -0,0 +1,12 @@
+class Musica():
+    nome = ''
+    artista = ''
+    duracao = ''
+
+
+track1 = Musica()
+track1.nome = 'praise jah in the moonlight'
+track1.artista = 'yg marley'
+track1.duracao = '4:23'
+
+print(vars(track1))

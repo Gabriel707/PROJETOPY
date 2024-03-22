e088d91d539f725aa8aa6a5aa08122c31af402f9
Gabriel Araujo <gabrielsant95@gmail.com>
2024-03-21 22:38:19 -0300

    Playing around with classes

--- python_crash_course_exercises/carro.py
+++ python_crash_course_exercises/carro.py
@@ -3,23 +3,24 @@ class Carro():
 
     carros = []
 
-    def __init__(self, marca, modelo, ano, cambio, carroceria, cor):
+    def __init__(self, marca, modelo, ano, cambio, carroceria, cor, vendido=False):
         self.marca = marca
         self.modelo = modelo
         self.ano = ano
         self.cambio = cambio
         self.carroceria = carroceria
         self.cor = cor
+        self.vendido = vendido
 
         Carro.carros.append(self)
 
     def __str__():
-        return f'{self.marca} | {self.modelo} | {self.ano} | {self.cambio} | {self.carroceria} | {self.cor}'
+        return f'{self.marca} | {self.modelo} | {self.ano} | {self.cambio} | {self.carroceria} | {self.cor} | {self.vendido}'
 
     def list_carro():
         for carro in Carro.carros:
             print(f"{carro.marca} | {carro.modelo} | {carro.ano} | {
-                  carro.cambio} | {carro.carroceria} | {carro.cor}")
+                  carro.cambio} | {carro.carroceria} | {carro.cor} | Vendido: {carro.vendido}")
 
 
 meu_carro = Carro('Mercedes', 'A250', '2019',
